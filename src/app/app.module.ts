96a1170bbdfc765e954c20892af838357bc6e47c
Delgado Trueba <delgadotrueba@outlook.es>
2019-02-26 15:16:36 +0100

    Material Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,31 @@
+//Modulos
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+//Services
+
+//Material Design
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
+//Components
 import { AppComponent } from './app.component';
 
 @NgModule({
+  //Importar Componenentes
   declarations: [
     AppComponent
   ],
+  //Importar Modulos, Material Design y Flexbox
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
+  //Importar Servicios
   providers: [],
   bootstrap: [AppComponent]
 })
