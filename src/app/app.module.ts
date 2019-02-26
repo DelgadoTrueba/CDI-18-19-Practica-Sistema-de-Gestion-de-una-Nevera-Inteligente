83f672ca646a7fa72e8ac8249b03e7832b781a01
Delgado Trueba <delgadotrueba@outlook.es>
2019-02-26 22:53:32 +0100

    navegador & tab1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,23 @@ import { AppRoutingModule } from './app-routing.module';
 //Material Design
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+
 
 //Importar Flex-Layout
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 //Components
 import { AppComponent } from './app.component';
+import { NavegadorComponent } from './components/navegador/navegador.component';
+import { Tab1Component } from './components/tab1/tab1.component';
 
 @NgModule({
   //Importar Componenentes
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegadorComponent,
+    Tab1Component
   ],
   //Importar Modulos, Material Design y Flexbox
   imports: [
@@ -27,6 +33,7 @@ import { AppComponent } from './app.component';
 
     BrowserAnimationsModule,
     MatButtonModule,
+    MatTabsModule,
     FlexLayoutModule
   ],
   //Importar Servicios
---
+++ src/app/components/navegador/navegador.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-navegador',
+  templateUrl: './navegador.component.html',
+  styleUrls: ['./navegador.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class NavegadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
