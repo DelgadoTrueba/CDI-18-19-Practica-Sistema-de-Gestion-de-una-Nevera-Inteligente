c1e8ad987ba1680bba76980468c1372e7901185f
Delgado Trueba <delgadotrueba@outlook.es>
2019-02-28 22:34:36 +0100

    fridge

--- src/app/components/fridge/fridge.component.ts
+++ src/app/components/fridge/fridge.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FridgeComponent implements OnInit {
 
-  constructor() { }
+  private cerrado: boolean;
+  private abieroArriba: boolean;
+  private abiertoAbajo: boolean;
+
+  constructor() {
+    this.cerrado = true;
+    this.abieroArriba = false;
+    this.abiertoAbajo = false;
+  }
 
   ngOnInit() {
+  }
+
+  public abrirArriba(){
+    this.abieroArriba = !this.abieroArriba;  
+    if(!this.abiertoAbajo) this.cerrado = !this.abieroArriba;
+  }
+
+
+  public abrirAbajo() {
+    this.abiertoAbajo = !this.abiertoAbajo;  
+    if(!this.abieroArriba) this.cerrado = !this.abiertoAbajo;
+  }
+
+  playSound(soundfile) {
+    document.getElementById("audio").innerHTML= 
+    "<embed src="+soundfile+" hidden='true' autostart='true' loop='false' />";
   }
 
 }
