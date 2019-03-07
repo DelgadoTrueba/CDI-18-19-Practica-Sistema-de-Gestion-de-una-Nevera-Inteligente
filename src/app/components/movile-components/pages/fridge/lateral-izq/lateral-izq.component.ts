eb52f494d0556a8ad3141f822c173e790f654c06
delgado.trueba <delgado.trueba@outlook.es>
2019-03-07 17:22:42 +0100

    zoom effect in sides

--- src/app/components/movile-components/pages/fridge/lateral-izq/lateral-izq.component.ts
+++ src/app/components/movile-components/pages/fridge/lateral-izq/lateral-izq.component.ts
@@ -64,14 +64,12 @@ export class LateralIzqComponent implements OnInit {
 
   calcularMapArea(){
     this.calcular1_primera();
-    this.calcular2();
-    this.calcular3();
     this.calcular4_ultima();
 
   }
 
   calcular1_primera(){
-    this.mapArea.A.x = this.height*(0.3);
+    this.mapArea.A.x = this.height*(0.33);
     this.mapArea.A.y = 0;
 
     this.mapArea.B.x = 0;
@@ -80,55 +78,24 @@ export class LateralIzqComponent implements OnInit {
     this.mapArea.C.x = 0;
     this.mapArea.C.y = this.width;
 
-    this.mapArea.D.x = this.height*(0.3) ;
+    this.mapArea.D.x = this.height*(0.33) ;
     this.mapArea.D.y = this.width ;
 
     this.mapCoorABCD = this.toStringCoor(this.mapArea.A, this.mapArea.B, this.mapArea.C, this.mapArea.D);
   }
 
-  calcular2(){
-    this.mapArea.E.x = this.height*(0.55);
-    this.mapArea.E.y = 0;
-
-    this.mapArea.F.x = this.height*(0.31);
-    this.mapArea.F.y = 0;
-
-    this.mapArea.G.x = this.height*(0.31);
-    this.mapArea.G.y = this.width;
-
-    this.mapArea.H.x = this.height*(0.55);
-    this.mapArea.H.y = this.width ;
-
-    this.mapCoorEFGH = this.toStringCoor(this.mapArea.E, this.mapArea.F, this.mapArea.G, this.mapArea.H);
-  }
-
-  calcular3(){
-    this.mapArea.I.x = this.height*(0.77);
-    this.mapArea.I.y = 0;
-
-    this.mapArea.J.x = this.height*(0.55);
-    this.mapArea.J.y = 0;
-
-    this.mapArea.K.x = this.height*(0.55);
-    this.mapArea.K.y = this.width;
-
-    this.mapArea.L.x = this.height*(0.77);
-    this.mapArea.L.y = this.width ;
-
-    this.mapCoorIJKL = this.toStringCoor(this.mapArea.I, this.mapArea.J, this.mapArea.K, this.mapArea.L);
-  }
-
+  
   calcular4_ultima(){
-    this.mapArea.M.x = this.height;
+    this.mapArea.M.x = this.height*(0.84);
     this.mapArea.M.y = 0;
 
-    this.mapArea.N.x = this.height*(0.77);
+    this.mapArea.N.x = this.height*(0.61);
     this.mapArea.N.y = 0;
 
-    this.mapArea.O.x = this.height*(0.77);
+    this.mapArea.O.x = this.height*(0.61);
     this.mapArea.O.y = this.width;
 
-    this.mapArea.P.x = this.height;
+    this.mapArea.P.x = this.height*(0.84);
     this.mapArea.P.y = this.width ;
 
     this.mapCoorMNOP = this.toStringCoor(this.mapArea.M, this.mapArea.N, this.mapArea.O, this.mapArea.P);
