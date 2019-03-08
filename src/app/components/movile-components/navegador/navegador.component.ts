a94f3fc5a803e2d7c6e154d39d5badf4d0a2846a
delgado.trueba <delgado.trueba@outlook.es>
2019-03-08 10:17:31 +0100

    nav var tamaños

--- src/app/components/movile-components/navegador/navegador.component.ts
+++ src/app/components/movile-components/navegador/navegador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation, ViewChild, Input } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild, Input, HostListener } from '@angular/core';
 import { MatTabGroup, MatTabChangeEvent } from '@angular/material/tabs';
 import { Router, NavigationEnd, } from '@angular/router';
 import { arrayDeNavegacion as tabs} from 'src/app/model/array-de-navegacion/array-de-navegacion.component';
@@ -14,6 +14,9 @@ export class NavegadorComponent implements OnInit {
   private tabs:Array<string> = tabs;
   @Input() position: number = 0;
 
+  height: number;
+  width: number;
+
 
   @ViewChild("tabGroup") tabGroup: MatTabGroup;
 
@@ -23,7 +26,17 @@ export class NavegadorComponent implements OnInit {
 
   ngOnInit() {
    this.reAlignTabGroup(this.position);
+   this.height = (window.innerHeight*0.10);
+   this.width  = (window.innerWidth);
+
   }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.height =(window.innerHeight*0.10);
+    this.width  = (window.innerWidth);
+  }
+
 
   ngOnChanges(changes) {
     //console.log('Changed', changes.position.currentValue, changes.position.previousValue);
