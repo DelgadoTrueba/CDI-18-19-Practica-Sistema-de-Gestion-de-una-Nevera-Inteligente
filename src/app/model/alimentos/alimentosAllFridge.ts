899b2dd783ec088285efd664085c2c932d45b6aa
delgado.trueba <delgadotrueba@outlook.es>
2019-03-21 13:23:40 +0100

    alimentosAllFridge

--- src/app/model/alimentos/alimentosAllFridge.ts
+++ src/app/model/alimentos/alimentosAllFridge.ts
@@ -1,33 +1,153 @@
 export let AlimentosAllFridge: Array<Alimento> = [
     {
-        nombreId: "Pera", 
-        rutaOk:"assets/img/all-fridge-con-alimentos/pera.png", 
-        rutaEmpty:"assets/img/all-fridge-con-alimentos/pera-cruz.png", 
-        cantidad: 5
+        nombreId: "Agua", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/agua.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/agua-cruz.png", 
+        cantidad: 0
     },
     {
-        nombreId: "Melon", 
-        rutaOk:"assets/img/all-fridge-con-alimentos/melon.png", 
-        rutaEmpty:"assets/img/all-fridge-con-alimentos/melon-cruz.png", 
-        cantidad: 5
+        nombreId: "Broccoli", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/broccoli.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/broccoli-cruz.png", 
+        cantidad: 0
     },
     {
-        nombreId: "Platano", 
-        rutaOk:"assets/img/all-fridge-con-alimentos/platano.png", 
-        rutaEmpty:"assets/img/all-fridge-con-alimentos/platano-cruz.png", 
-        cantidad: 5
-    },
-    {
-        nombreId: "Manzana", 
-        rutaOk:"assets/img/all-fridge-con-alimentos/manzana.png", 
-        rutaEmpty:"assets/img/all-fridge-con-alimentos/manzana-cruz.png", 
-        cantidad: 5
+        nombreId: "Carne", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/carne.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/carne-cruz.png", 
+        cantidad: 0
     },
     {
         nombreId: "Cereza", 
         rutaOk:"assets/img/all-fridge-con-alimentos/cherry.png", 
         rutaEmpty:"assets/img/all-fridge-con-alimentos/cherry-cruz.png", 
-        cantidad: 5
+        cantidad: 0
+    },
+    {
+        nombreId: "CocaCola", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/coca-cola.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/coca-cola-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Coliflor", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/coliflor.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/coliflor-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Fanta", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/fanta.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/fanta-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Huevos", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/huevo.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/huevo-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Leche", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/leche.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/leche-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Mantequilla", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/mantequilla.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/mantequilla-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Manzana", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/manzana.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/manzana-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Melon", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/melon.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/melon-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Pepino", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/pepino.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/pepino-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Pera", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/pera.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/pera-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Pescado", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/pescado.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/pescado-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Pimiento", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/pimiento.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/pimiento-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Platano", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/platano.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/platano-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Pollo", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/pollo.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/pollo-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Queso", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/queso.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/queso-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Salchicha", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/salchicha.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/salchicha-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "SalsaDeTomate", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/salsa-tomate.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/salsa-tomate-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Sprite", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/sprite.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/sprite-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Yogurt", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/yogurt.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/yogurt-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Zanahoria", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/zanahoria.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/zanahoria-cruz.png", 
+        cantidad: 0
+    },
+    {
+        nombreId: "Zumo de Naranga", 
+        rutaOk:"assets/img/all-fridge-con-alimentos/zumo.png", 
+        rutaEmpty:"assets/img/all-fridge-con-alimentos/zumo-cruz.png", 
+        cantidad: 0
     },
 
 
