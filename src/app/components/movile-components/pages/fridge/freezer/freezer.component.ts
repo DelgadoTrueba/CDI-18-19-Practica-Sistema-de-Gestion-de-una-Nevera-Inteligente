092cbe93cb14ab409e85e26a2631917db5dc48b3
delgado.trueba <delgado.trueba@outlook.es>
2019-04-06 20:11:32 +0200

    freezer

--- src/app/components/movile-components/pages/fridge/freezer/freezer.component.ts
+++ src/app/components/movile-components/pages/fridge/freezer/freezer.component.ts
@@ -53,8 +53,6 @@ export class FreezerComponent implements OnInit, OnDestroy {
     this.width = ( window.innerHeight >= window.innerWidth ) ? (window.innerWidth*0.8) : 350;
     
     //console.log("width: "+ this.width+", "+"height: "+ this.height );
-
-    this.calcularMapArea();
   }
 
   @HostListener('window:resize', ['$event'])
@@ -63,141 +61,21 @@ export class FreezerComponent implements OnInit, OnDestroy {
     this.width = ( window.innerHeight >= window.innerWidth ) ? (window.innerWidth*0.8) : 350;
     
     //console.log("width: "+ this.width+", "+"height: "+ this.height );
-
-    this.calcularMapArea();
   }
 
   ngOnDestroy() {
     this.subscribeServiceAlimentos.unsubscribe();
   }
 
-  calcularMapArea(){
-    this.calcularHelado();
-    this.calcularChurro();
-    this.calcularTarta();
-    this.calcularPizza();
-    this.calcularPatata();
-    this.calcularVerduras();
-    this.calcularPescado();
-    this.calcularCarne();
-  }
-
-  calcularHelado(){
-    let coord1x, coord1y, coord2x, coord2y;
-
-    coord1x = Math.round( this.width * 0.11 ); coord1y = Math.round( this.height * 0.11 ); coord2x = Math.round( this.width * 0.27 ); coord2y = Math.round( this.height * 0.28 ); 
-    
-    let mapCoord = [ coord1x, coord1y, coord2x, coord2y];
-    
-    this.mapCoorHelado = mapCoord.reduce( (v_ant, v_act, index,)=>{if(index ==0 ) return v_ant += `${v_act} `;else  return v_ant += `,${v_act} `;}, '');
-  }
-  calcularChurro(){
- let coord1x, coord1y, coord2x, coord2y;
-
-  coord1x = Math.round( this.width * 0.4 ); coord1y = Math.round( this.height * 0.06 ); coord2x = Math.round( this.width * 0.55 ); coord2y = Math.round( this.height * 0.29 ); 
-
-  let mapCoord = [ coord1x, coord1y, coord2x, coord2y];
-
-  this.mapCoorChurro = mapCoord.reduce( (v_ant, v_act, index,)=>{if(index ==0 ) return v_ant += `${v_act} `;else  return v_ant += `,${v_act} `;}, '');
-  }
-  calcularTarta(){
-  let coord1x, coord1y, coord2x, coord2y;
-
-  coord1x = Math.round( this.width * 0.68 ); coord1y = Math.round( this.height * 0.1 ); coord2x = Math.round( this.width * 0.85 ); coord2y = Math.round( this.height * 0.28 ); 
-
-  let mapCoord = [ coord1x, coord1y, coord2x, coord2y];
-
-  this.mapCoorTarta = mapCoord.reduce( (v_ant, v_act, index,)=>{if(index ==0 ) return v_ant += `${v_act} `;else  return v_ant += `,${v_act} `;}, '');
-  }
-  calcularPizza(){
-  let coord1x, coord1y, coord2x, coord2y;
-
-  coord1x = Math.round( this.width * 0.13 ); coord1y = Math.round( this.height * 0.37 ); coord2x = Math.round( this.width * 0.29 ); coord2y = Math.round( this.height * 0.54 ); 
-
-  let mapCoord = [ coord1x, coord1y, coord2x, coord2y];
-
-  this.mapCoorPizza = mapCoord.reduce( (v_ant, v_act, index,)=>{if(index ==0 ) return v_ant += `${v_act} `;else  return v_ant += `,${v_act} `;}, '');
-  }
-  calcularPatata(){
-  let coord1x, coord1y, coord2x, coord2y;
-
-  coord1x = Math.round( this.width * 0.39 ); coord1y = Math.round( this.height * 0.31 ); coord2x = Math.round( this.width * 0.56 ); coord2y = Math.round( this.height * 0.56 ); 
-
-  let mapCoord = [ coord1x, coord1y, coord2x, coord2y];
-
-  this.mapCoorPatata = mapCoord.reduce( (v_ant, v_act, index,)=>{if(index ==0 ) return v_ant += `${v_act} `;else  return v_ant += `,${v_act} `;}, '');
-  }
-  calcularVerduras(){
-  let coord1x, coord1y, coord2x, coord2y;
-
-  coord1x = Math.round( this.width * 0.65 ); coord1y = Math.round( this.height * 0.31 ); coord2x = Math.round( this.width * 0.88 ); coord2y = Math.round( this.height * 0.56 ); 
-
-  let mapCoord = [ coord1x, coord1y, coord2x, coord2y];
-
-  this.mapCoorVerduras = mapCoord.reduce( (v_ant, v_act, index,)=>{if(index ==0 ) return v_ant += `${v_act} `;else  return v_ant += `,${v_act} `;}, '');
-  }
-  calcularPescado(){
-  let coord1x, coord1y, coord2x, coord2y;
-
-  coord1x = Math.round( this.width * 0.21 ); coord1y = Math.round( this.height * 0.65 ); coord2x = Math.round( this.width * 0.47 ); coord2y = Math.round( this.height * 0.84 ); 
-
-  let mapCoord = [ coord1x, coord1y, coord2x, coord2y];
-
-  this.mapCoorPescado = mapCoord.reduce( (v_ant, v_act, index,)=>{if(index ==0 ) return v_ant += `${v_act} `;else  return v_ant += `,${v_act} `;}, '');
-  }
-  calcularCarne(){
-  let coord1x, coord1y, coord2x, coord2y;
-
-  coord1x = Math.round( this.width * 0.57 ); coord1y = Math.round( this.height * 0.67 ); coord2x = Math.round( this.width * 0.76 ); coord2y = Math.round( this.height * 0.84 ); 
-
-  let mapCoord = [ coord1x, coord1y, coord2x, coord2y];
-
-  this.mapCoorCarne = mapCoord.reduce( (v_ant, v_act, index,)=>{if(index ==0 ) return v_ant += `${v_act} `;else  return v_ant += `,${v_act} `;}, '');
-  }
-
-
   abrirCajon1(){
-    this.mostradorDeAlimentosComponent.abrirPanel1();
+    this.arrayCajonIsOpen[0] = !this.arrayCajonIsOpen[0];
   }
   abrirCajon2(){
-    this.mostradorDeAlimentosComponent.abrirPanel2();
+    this.arrayCajonIsOpen[1] = !this.arrayCajonIsOpen[1];  
   }
   abrirCajon3(){
-    this.mostradorDeAlimentosComponent.abrirPanel3();
-  }
-
-  cerrarCajon1(){
-    this.arrayCajonIsOpen[0] = false ;
-  }
-
-  abrirCajon1Aux(){
-    this.arrayCajonIsOpen[0] = true ;
-  }
-
-  cerrarCajon2(){
-    this.arrayCajonIsOpen[1] = false ;
-  }
-
-  abrirCajon2Aux(){
-    this.arrayCajonIsOpen[1] = true ;
-  }
-
-  cerrarCajon3(){
-    this.arrayCajonIsOpen[2] = false ;
-  }
-
-  abrirCajon3Aux(){
-    this.arrayCajonIsOpen[2] = true ;
-  }
-
-  //A largo(X) B ancho(y)
-  toStringCoor(ptoA, ptoB, ptoC, ptoD){
-    
-    return +parseInt(ptoA.y , 10) +","+ parseInt(ptoA.x, 10) +
-      " "+ parseInt(ptoB.y, 10) +","+ parseInt(ptoB.x, 10) +
-      " "+ parseInt(ptoC.y, 10) +","+ parseInt(ptoC.x, 10) +
-      " "+ parseInt(ptoD.y, 10) +","+ parseInt(ptoD.x, 10);
-  }
+    this.arrayCajonIsOpen[2] = !this.arrayCajonIsOpen[2];
+    }
 
 
   openDialog(alimentoNombre) {
