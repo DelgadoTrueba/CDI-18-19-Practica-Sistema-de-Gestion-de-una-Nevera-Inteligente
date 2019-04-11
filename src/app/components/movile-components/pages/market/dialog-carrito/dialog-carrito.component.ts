776ad4c983d092e6355f89d8e96f5f8fd3cdf6a9
delgado.trueba <delgado.trueba@outlook.es>
2019-04-11 12:10:13 +0200

    alta

--- src/app/components/movile-components/pages/market/dialog-carrito/dialog-carrito.component.ts
+++ src/app/components/movile-components/pages/market/dialog-carrito/dialog-carrito.component.ts
@@ -3,6 +3,7 @@ import { CarritoService } from 'src/app/services/carrito.service';
 import { MatDialogConfig, MatDialog, MatTooltip } from '@angular/material';
 import { DialogCancelComponent } from 'src/app/components/core/dialog-cancel/dialog-cancel.component';
 import { DialogModificarComponent } from '../dialog-modificar/dialog-modificar.component';
+import { GlobalService } from 'src/app/services/global.service';
 
 @Component({
   selector: 'app-dialog-carrito',
@@ -15,12 +16,20 @@ export class DialogCarritoComponent implements OnInit {
   displayedColumns: string[] = ['nombre', 'cantidad', "precio", "actions"];
   total: number;
 
+  foods= [
+    {value: '1234-5678-9101-112', viewValue: '1234-5678-9101-112'},
+  ];
+  alta: boolean;
+
   constructor(
     private carritoServicie: CarritoService,
-    private dialog :MatDialog
+    private dialog :MatDialog,
+    private globalServicie: GlobalService
   ) { }
 
   ngOnInit() {
+    
+    this.alta = this.globalServicie.getAlta();
     
       this.carritoServicie.notification$.subscribe( (alimentos)=>{
         this.alimentosEnCarrito = alimentos.filter( (alimento)=>{
