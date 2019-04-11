de00f127f180a5517dc391ae2e577aa2a906ec27
delgado.trueba <delgado.trueba@outlook.es>
2019-04-11 22:42:15 +0200

    recarga de depositos

--- src/app/components/movile-components/pages/fridge/dialog-agua/dialog-agua.component.ts
+++ src/app/components/movile-components/pages/fridge/dialog-agua/dialog-agua.component.ts
@@ -1,5 +1,5 @@
-import {MatDialog, MatDialogConfig, MatDialogRef} from "@angular/material";
-import { Component } from '@angular/core';
+import {MatDialog, MatDialogConfig, MatDialogRef, MatSlideToggle} from "@angular/material";
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-DialogoAgua',
@@ -10,10 +10,26 @@ import { Component } from '@angular/core';
 export class  DialogAguaComponent{
 
     toggleIsOn = true;
+    a0 = false;
+    a10 = false;
+    a20 = false;
+    a40 = false;
+    a60 = false;
+    a80 = false;
+    a100 = true;
+    number: number = 0;
+
+    abierto = false;
+    pid;
 
     constructor(
         private dialog: MatDialog,
         private dialogRef: MatDialogRef<DialogAguaComponent>,) {}
+
+
+    OnInit(){
+      
+    }
 
     openDialog() {
 
@@ -30,8 +46,38 @@ export class  DialogAguaComponent{
     this.dialogRef.close();
   }
 
+  @ViewChild("grifo") grifo: MatSlideToggle;
     changeImg()
     {
-      this.toggleIsOn = !this.toggleIsOn;
+      if(this.abierto===true) {
+        clearTimeout(this.pid);        
+        this.toggleIsOn = true;  
+        this.abierto = false;
+        return;}
+      
+
+      this.abierto = true;
+      this.toggleIsOn = false;
+      this.pid= setInterval( ()=>{
+        this.number++;
+        if(this.number === 1) {this.a100 = false; this.a80 = true;}
+        if(this.number === 2) {this.a80 = false; this.a60 = true;}
+        if(this.number === 3) {this.a60 = false; this.a40 = true;}
+        if(this.number === 4) {this.a40 = false; this.a20 = true;}
+        if(this.number === 5) {this.a20 = false; this.a0 = true; this.toggleIsOn = true; 
+        this.number = 0; this.grifo.toggle(); this.abierto = false; clearTimeout(this.pid);   
+      }
+        console.log(this.number)
+      },1000)
+    }
+
+    reset(){
+      this.a0 = false;
+    this.a10 = false;
+    this.a20 = false;
+    this.a40 = false;
+    this.a60 = false;
+    this.a80 = false;
+    this.a100 = true;
     }
 }
--- src/app/components/movile-components/pages/fridge/dialog-hielo/dialog-hielo.component.ts
+++ src/app/components/movile-components/pages/fridge/dialog-hielo/dialog-hielo.component.ts
@@ -1,40 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-import {MatDialog, MatDialogConfig, MatDialogRef} from "@angular/material";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatDialog, MatDialogConfig, MatDialogRef, MatSlideToggle} from "@angular/material";
 
 @Component({
   selector: 'app-dialog-hielo',
   templateUrl: './dialog-hielo.component.html',
   styleUrls: ['./dialog-hielo.component.css']
 })
-export class DialogHieloComponent implements OnInit {
+export class DialogHieloComponent {
 
+  
   toggleIsOn = true;
+  a0 = false;
+  a10 = false;
+  a20 = false;
+  a40 = false;
+  a60 = false;
+  a80 = false;
+  a100 = true;
+  number: number = 0;
+
+  abierto = false;
+  pid;
 
   constructor(
-    private dialog: MatDialog,
-    private dialogRef: MatDialogRef<DialogHieloComponent>,) {}
+      private dialog: MatDialog,
+      private dialogRef: MatDialogRef<DialogHieloComponent>,) {}
 
-  ngOnInit() {
+
+  OnInit(){
+    
   }
 
   openDialog() {
 
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-
-    this.dialog.open(DialogHieloComponent, dialogConfig);
+   
   }
 
   close()
+{
+  this.dialogRef.close();
+}
+
+@ViewChild("grifo") grifo: MatSlideToggle;
+  changeImg()
   {
-    this.dialogRef.close();
+    if(this.abierto===true) {
+      clearTimeout(this.pid);        
+      this.toggleIsOn = true;  
+      this.abierto = false;
+      return;}
+    
+
+    this.abierto = true;
+    this.toggleIsOn = false;
+    this.pid= setInterval( ()=>{
+      this.number++;
+      if(this.number === 1) {this.a100 = false; this.a80 = true;}
+      if(this.number === 2) {this.a80 = false; this.a60 = true;}
+      if(this.number === 3) {this.a60 = false; this.a40 = true;}
+      if(this.number === 4) {this.a40 = false; this.a20 = true;}
+      if(this.number === 5) {this.a20 = false; this.a0 = true; this.toggleIsOn = true; 
+      this.number = 0; this.grifo.toggle(); this.abierto = false; clearTimeout(this.pid);   
+    }
+      console.log(this.number)
+    },1000)
   }
 
-  changeImg()
-    {
-      this.toggleIsOn = !this.toggleIsOn;
-    }
-
+  reset(){
+    this.a0 = false;
+  this.a10 = false;
+  this.a20 = false;
+  this.a40 = false;
+  this.a60 = false;
+  this.a80 = false;
+  this.a100 = true;
+  }
 }
